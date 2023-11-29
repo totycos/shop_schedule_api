290188d8affc037ee464e1e2471146e01323beb2
totycos <dv94@hotmail.fr>
2023-11-29 23:28:28 +0100

    build(shop): Add validations and association in the model + update tests

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# Model representing a shop in the application.
 class Shop < ApplicationRecord
+  has_many :schedules, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  # spec/models/shop_spec.rb
-  require 'rails_helper'
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+    it { should_not allow_value('').for(:name) }
+  end
 
-  RSpec.describe Shop, type: :model do
-    context 'validations' do
-      it { should validate_presence_of(:name) }
-      it { should validate_uniqueness_of(:name).case_insensitive }
-      it { should_not allow_value('').for(:name) }
+  context 'associations' do
+    it { should have_many(:schedules).dependent(:destroy) }
 
-      context 'when a shop with the same name exists' do
-        create(:shop, name: 'My Shop')
-        it { should_not validate_uniqueness_of(:name).case_insensitive }
-      end
+    it 'destroys associated schedules when shop is destroyed' do
+      shop = create(:shop)
+      schedule = create(:schedule, shop: shop)
+
+      expect { shop.destroy }.to change(Schedule, :count).by(-1)
     end
   end
 end
