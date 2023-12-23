76860b4d36844a1ca83685e80a75e9bd9efef228
totycos <dv94@hotmail.fr>
2023-12-23 01:10:42 +0100

    refactor: Shops controller tests

--- spec/requests/api/v1/shops_spec.rb
+++ spec/requests/api/v1/shops_spec.rb
@@ -3,40 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe '/api/v1/shops', type: :request do
-  before do
-    @shop = create(:shop, id: 1)
-    @shop2 = create(:shop, id: 2)
-    @schedule = create(:schedule, day: 'Monday', opening_time: '08:00', closing_time: '12:00', shop: @shop)
-    @schedule2 = create(:schedule, day: 'Monday', opening_time: '13:00', closing_time: '18:00', shop: @shop)
-  end
-
   describe 'GET /index' do
     it 'return http success' do
       get api_v1_shops_url
       expect(response).to have_http_status(:success)
     end
 
-    it 'returns all shops with associated schedules' do
+    it 'returns all shops with associated schedules' do # rubocop:disable RSpec/ExampleLength,RSpec/MultipleExpectations
+      shop = create(:shop)
+      create(:schedule, shop: shop)
+      create(:schedule, shop: shop)
+      shop2 = create(:shop)
+
       get api_v1_shops_url, as: :json
       json = JSON.parse(response.body)
+
       expect(json.size).to eq(2)
-      expect(json[0]['name']).to eq(@shop.name)
+      expect(json[0]['name']).to eq(shop.name)
       expect(json[0]['sorted_schedules'].size).to eq(2)
-      expect(json[1]['name']).to eq(@shop2.name)
+      expect(json[1]['name']).to eq(shop2.name)
       expect(json[1]['sorted_schedules'].size).to eq(0)
     end
   end
 
   describe 'GET /show' do
     it 'return http success' do
-      get api_v1_shop_url(@shop), as: :json
+      shop = create(:shop)
+      get api_v1_shop_url(shop), as: :json
       expect(response).to have_http_status(:success)
     end
 
-    it 'returns the shop with associated schedules' do
-      get api_v1_shop_url(@shop), as: :json
+    it 'returns the shop with associated schedules' do # rubocop:disable RSpec/ExampleLength,RSpec/MultipleExpectations
+      shop = create(:shop)
+      create(:schedule, shop: shop)
+      create(:schedule, shop: shop)
+
+      get api_v1_shop_url(shop), as: :json
       json = JSON.parse(response.body)
-      expect(json['name']).to eq(@shop.name)
+
+      expect(json['name']).to eq(shop.name)
       expect(json['sorted_schedules'].size).to eq(2)
     end
   end
@@ -83,7 +88,8 @@ RSpec.describe '/api/v1/shops', type: :request do
   describe 'PATCH /update' do
     context 'with valid parameters' do
       before do
-        patch api_v1_shop_url(@shop), params: {
+        shop = create(:shop)
+        patch api_v1_shop_url(shop), params: {
           api_v1_shop: {
             name: 'UpdatedShopName'
           }
@@ -101,7 +107,8 @@ RSpec.describe '/api/v1/shops', type: :request do
 
     context 'with invalid parameters' do
       before do
-        patch api_v1_shop_url(@shop), params: {
+        shop = create(:shop)
+        patch api_v1_shop_url(shop), params: {
           api_v1_shop: {
             name: ''
           }
@@ -116,15 +123,16 @@ RSpec.describe '/api/v1/shops', type: :request do
 
   describe 'DELETE /destroy' do
     it 'destroys the requested shop' do
-      expect do
-        delete api_v1_shop_url(@shop), as: :json
-      end.to change(Shop, :count).by(-1)
+      shop = create(:shop)
+      expect { delete api_v1_shop_url(shop), as: :json }.to change(Shop, :count).by(-1)
     end
 
     it 'destroys associated schedules' do
-      expect do
-        delete api_v1_shop_url(@shop), as: :json
-      end.to change(Schedule, :count).by(-2)
+      shop = create(:shop)
+      create(:schedule, shop: shop)
+      create(:schedule, shop: shop)
+
+      expect { delete api_v1_shop_url(shop), as: :json }.to change(Schedule, :count).by(-2)
     end
   end
 end
