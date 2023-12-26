73e7a0a6d5b6e86587459232509d08a5a15e78fa
totycos <dv94@hotmail.fr>
2023-12-26 15:29:10 +0100

    test: Add routes tests

--- spec/routing/api/v1/schedules_routing_spec.rb
+++ spec/routing/api/v1/schedules_routing_spec.rb
@@ -5,27 +5,27 @@ require 'rails_helper'
 RSpec.describe Api::V1::SchedulesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/api/v1/schedules').to route_to('api/v1/schedules#index')
+      expect(get: '/api/v1/shops/1/schedules').to route_to('api/v1/schedules#index', shop_id: '1')
     end
 
     it 'routes to #show' do
-      expect(get: '/api/v1/schedules/1').to route_to('api/v1/schedules#show', id: '1')
+      expect(get: '/api/v1/shops/1/schedules/1').to route_to('api/v1/schedules#show', shop_id: '1', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/api/v1/schedules').to route_to('api/v1/schedules#create')
+      expect(post: '/api/v1/shops/1/schedules').to route_to('api/v1/schedules#create', shop_id: '1')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/api/v1/schedules/1').to route_to('api/v1/schedules#update', id: '1')
+      expect(put: '/api/v1/shops/1/schedules/1').to route_to('api/v1/schedules#update', shop_id: '1', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/api/v1/schedules/1').to route_to('api/v1/schedules#update', id: '1')
+      expect(patch: '/api/v1/shops/1/schedules/1').to route_to('api/v1/schedules#update', shop_id: '1', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/api/v1/schedules/1').to route_to('api/v1/schedules#destroy', id: '1')
+      expect(delete: '/api/v1/shops/1/schedules/1').to route_to('api/v1/schedules#destroy', shop_id: '1', id: '1')
     end
   end
 end
